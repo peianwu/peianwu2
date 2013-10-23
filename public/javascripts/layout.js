003e65a104f2f5868a2c97a3f39ddde1dc3b784b
efkztw <peian.peter.wu@gmail.com>
2013-10-23 08:44:07 +0800

    fixed loading jquery lib

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,3 +1,10 @@
+// detect jquery source availability
+// if google hosting not available revert to local
+
+if (!window.jQuery) {
+  document.write('<script src="/javascripts/lib/jquery-1.10.2.min.js"></script>');
+}
+
 (function() {
 
   window.onload = function() {
